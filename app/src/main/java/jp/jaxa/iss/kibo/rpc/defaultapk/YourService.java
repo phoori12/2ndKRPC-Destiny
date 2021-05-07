7825485a05097f2fa988cf9988035e58ec6b866e
phoori12 <phoori12@gmail.com>
2021-05-08 00:33:54 +0700

    Initial Commit

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -0,0 +1,313 @@
+package jp.jaxa.iss.kibo.rpc.defaultapk;
+
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
+import gov.nasa.arc.astrobee.Result;
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
+
+import jp.jaxa.iss.kibo.rpc.api.types.PointCloud;
+
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.util.Log;
+
+import net.sourceforge.zbar.Config;
+import net.sourceforge.zbar.Image;
+import net.sourceforge.zbar.ImageScanner;
+import net.sourceforge.zbar.Symbol;
+import net.sourceforge.zbar.SymbolSet;
+
+import org.opencv.android.Utils;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.Core;
+import org.opencv.core.CvException;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+import org.opencv.aruco.Aruco;
+import org.opencv.aruco.DetectorParameters;
+import org.opencv.aruco.Dictionary;
+
+
+
+/**
+ * Class meant to handle commands from the Ground Data System and execute them in Astrobee
+ */
+
+public class YourService extends KiboRpcService {
+
+    Mat QR_snap;
+    Mat AR_snap;
+
+    @Override
+    protected void runPlan1(){
+
+        String QRPointA = null;
+        double QRData[]; // KOZ Pattern // A'X // A'Y // A'Z
+        int max_try = 4;
+        int try_read = 0;
+        int AR_cast = 0;
+        int sleep_time = 3;
+        double ncOffset_y = 0.042 ,ncOffset_z  = 0.083;
+
+        // Mission Start //
+        api.startMission();
+
+        //moveToWrapper(10.5,-9.8,4.6,0,0,-0.707,0.707,0);
+        moveToWrapper(11.21+ncOffset_y,-9.6,4.79+ncOffset_z,0,0,-0.707,0.707,5);
+        try {
+            sleep(sleep_time);
+        } catch (Exception e) {
+            Log.d("Sleep status", "sleep failed");
+        }
+        /////////////////////////////////////////////////////////////////////////////
+
+        while(QRPointA == null && try_read < max_try) {
+            QR_snap = api.getMatNavCam();
+
+            Bitmap QR_Bitmap = null;
+            Mat QR_mat = imProveImaging(QR_snap, false);
+            Log.d("IMGPROC", "Bitmap conversion started");
+            try {
+                QR_Bitmap = Bitmap.createBitmap(QR_mat.cols(), QR_mat.rows(), Bitmap.Config.ARGB_8888);
+                Utils.matToBitmap(QR_mat, QR_Bitmap);
+                Log.d("IMGPROC", "Bitmap conversion finished");
+                QRPointA = QRscaner(QR_Bitmap);
+            }
+            catch (CvException e){Log.d("Exception",e.getMessage());}
+
+
+           // Bitmap QR_crop = Bitmap.createBitmap(QR_snap,0,0,960,960);
+
+            try_read++;
+            //////////////////////////////////////////////////////////////////////////////////////
+        }
+        Log.d("QR", QRPointA);
+        api.sendDiscoveredQR(QRPointA);
+        QRData = parseQRinfo(QRPointA);
+        Log.d("QR", ""+ QRData[0] + ", " + QRData[1] + ", "+ QRData[2] + ", "+ QRData[3]);
+        AR_scanAndLocalize(false);
+        //moveToWrapper(QRData[1],QRData[2],QRData[3],0,0,-0.707,0.707,0);
+
+
+        api.reportMissionCompletion();
+
+    }
+
+    @Override
+    protected void runPlan2(){
+        // write here your plan 2
+    }
+
+    @Override
+    protected void runPlan3(){
+        // write here your plan 3
+    }
+
+    public void AR_scanAndLocalize(boolean status) {
+        int row = 0, col = 0;
+        double[][] NavCamInst = api.getNavCamIntrinsics();
+        Mat cameraMatrix = new Mat(3, 3, CvType.CV_32FC1);
+        Mat distCoeffs = new Mat(1, 5, CvType.CV_32FC1);
+        Mat dst = new Mat(1280, 960, CvType.CV_8UC1);
+        cameraMatrix.put(row, col, NavCamInst[0]);
+        distCoeffs.put(row, col, NavCamInst[1]);
+        Log.d("cameraMatrix", cameraMatrix.dump());
+        Log.d("distCoeffs", distCoeffs.dump());
+
+        Mat ids = new Mat();
+        List<Mat> corners = new ArrayList<>();
+        List<Mat> rej = new ArrayList<>();
+        double result = 0.0;
+//        Log.d("AR", "Started undistorting AR");
+
+//        Imgproc.undistort(AR_snap, dst, cameraMatrix, distCoeffs);
+//        Log.d("AR", "Finished undistorting AR");
+
+        Log.d("AR", "Started reading AR");
+        while (result == 0) {
+            AR_snap = api.getMatNavCam();
+            Mat AR_mat = imProveImaging(AR_snap, false);
+            try {
+                DetectorParameters parameters = DetectorParameters.create();
+//                parameters.set_errorCorrectionRate(1);
+                if (status) {
+                    parameters.set_adaptiveThreshWinSizeMin(5);
+                    parameters.set_adaptiveThreshWinSizeMax(29);
+                    parameters.set_adaptiveThreshWinSizeStep(4);
+                }
+                Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
+                Aruco.detectMarkers(AR_mat, dictionary, corners, ids, parameters,rej,cameraMatrix,distCoeffs);
+                if (!ids.empty()) {
+                    result = ids.get(0, 0)[0];
+                    Log.d("AR_IDs", "Result : " + result);
+                    Log.d("AR_IDs", ids.dump());
+                    break;
+                }
+            } catch (Exception e) {
+                Log.d("AR discover:", "Not detected");
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public double[] parseQRinfo(String QRData)
+    {
+        String[] remnants = QRData.split(",");
+
+        double parsedDouble[] = new double[4];
+        for (int i = 0;i< remnants.length;i++) {
+            parsedDouble[i] = Double.parseDouble(remnants[i].replaceAll("[^0-9. ]", ""));
+        }
+
+        return parsedDouble;
+    }
+
+    public void sleep(int timer) throws InterruptedException
+    {
+        TimeUnit.SECONDS.sleep(timer);
+    }
+
+    public String QRscaner(Bitmap source) // removed static
+    {
+        String result = null;
+
+        int[] pixel = new int[source.getWidth()*source.getHeight()];
+        source.getPixels(pixel, 0, source.getWidth(), 0, 0, source.getWidth(), source.getHeight());
+        Image barcode = new Image(source.getWidth() ,source.getHeight() ,"RGB4");
+        barcode.setData(pixel);
+        ImageScanner reader = new ImageScanner();
+        reader.setConfig(Symbol.NONE, Config.ENABLE, 0);
+        reader.setConfig(Symbol.QRCODE, Config.ENABLE, 1);
+        reader.scanImage(barcode.convert("Y800"));
+
+        try
+        {
+            SymbolSet syms = reader.getResults();
+            for(Symbol sym : syms)
+            {
+                result = sym.getData();
+            }
+        }
+        catch(Exception e)
+        {
+            Log.d("QR discover: ","Not detected");
+        }
+        return result;
+    }
+
+
+    public  Mat imProveImaging(Mat source, boolean undistort_state) {
+        int row = 0, col = 0;
+        double[][] NavCamInst = api.getNavCamIntrinsics();
+        Mat cameraMatrix = new Mat(3, 3, CvType.CV_32FC1);
+        Mat distCoeffs = new Mat(1, 5, CvType.CV_32FC1);
+        Mat dst = new Mat(1280, 960, CvType.CV_8UC1);
+        cameraMatrix.put(row, col, NavCamInst[0]);
+        distCoeffs.put(row, col, NavCamInst[1]);
+        Log.d("cameraMatrix", cameraMatrix.dump());
+        Log.d("distCoeffs", distCoeffs.dump());
+        Mat Thres = new Mat();
+        Mat result = new Mat();
+        if (undistort_state) {
+            Log.d("IMGPROC", "Undistortion process started");
+            Imgproc.undistort(source, dst, cameraMatrix, distCoeffs);
+            Log.d("IMGPROC", "Undistortion process finished");
+        } else {
+            Log.d("IMGPROC", "Skipping Undistortion process");
+            dst = source;
+        }
+
+        Log.d("IMGPROC", "Threshold process started");
+        Imgproc.threshold(dst, Thres, 240, 255, Imgproc.THRESH_TOZERO);
+        Log.d("IMGPROC", "Threshold process finished");
+        Log.d("IMGPROC", "Contour location process started");
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        List<MatOfPoint> Wanted_contours = new ArrayList<MatOfPoint>();
+        Imgproc.findContours(Thres, contours, new Mat(), Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_SIMPLE);
+        for(int i=0; i< contours.size();i++)
+        {
+            if ((Imgproc.contourArea(contours.get(i)) >7000))//Imgproc.contourArea(contours.get(i)) > 50
+            {
+                Wanted_contours.add(contours.get(i));
+                System.out.println(contours.get(i).dump());
+            }
+        }
+        Log.d("IMGPROC", "Contour location process finished");
+        Log.d("IMGPROC", "Filling process started");
+        Mat mask = new Mat( new Size( dst.cols(), dst.rows() ), CvType.CV_8UC1 );
+        Imgproc.drawContours(dst, Wanted_contours, -1, new Scalar(255,255,255), 3);
+        Imgproc.fillPoly(mask , Wanted_contours, new Scalar(255, 255, 255));
+        Core.bitwise_and(dst,mask,result);
+        Log.d("IMGPROC", "Filling process finished");
+
+        return result;
+    }
+
+
+    private void moveToWrapper(double pos_x, double pos_y, double pos_z,
+                               double qua_x, double qua_y, double qua_z,
+                               double qua_w, final int LOOP_MAX)
+    {
+        final Point point = new Point(pos_x, pos_y, pos_z);
+        final Quaternion quaternion = new Quaternion((float)qua_x, (float)qua_y,
+                (float)qua_z, (float)qua_w);
+
+        Result result = api.moveTo(point, quaternion, true);
+
+        int loopCounter = 0;
+        while(!result.hasSucceeded() || loopCounter < LOOP_MAX){
+            result = api.moveTo(point, quaternion, true);
+            ++loopCounter;
+        }
+    }
+
+
+    private Quaternion rotationCalculator(double x, double y, double z, double xp, double yp,double zp)
+    {
+
+
+        double x_sub,y_sub,z_sub,x_deg,z_deg;
+        x_sub  = xp-x; // + is right // - is left
+        y_sub = Math.abs(yp-y); // always absolute
+
+        double bftan = x_sub/y_sub;
+        z_deg = Math.toDegrees(Math.atan(bftan));
+        double z_deg_final = z_deg - 90;
+
+        z_sub = zp-z; // + is down // - is up
+        bftan = z_sub/y_sub;
+        x_deg = -Math.toDegrees(Math.atan(bftan));
+
+        z_deg = Math.toRadians(z_deg_final);
+        x_deg = Math.toRadians(x_deg);
+        double cy = Math.cos(z_deg * 0.5);
+        double sy = Math.sin(z_deg * 0.5);
+        double cp = Math.cos(0 * 0.5);
+        double sp = Math.sin(0 * 0.5);
+        double cr = Math.cos(x_deg * 0.5);
+        double sr = Math.sin(x_deg * 0.5);
+
+        double qua_w = cr * cp * cy + sr * sp * sy;
+        double qua_x = sr * cp * cy - cr * sp * sy;
+        double qua_y = -(cr * sp * cy + sr * cp * sy);
+        double qua_z = cr * cp * sy - sr * sp * cy;
+        Quaternion q = new Quaternion((float)qua_x,(float)qua_y,(float)qua_z,(float)qua_w);
+
+
+        return q;
+    }
+
+}
+
