d8bc72089071cdf9b2eb9454c0884eb3c2ddc2c7
phoori12 <phoori12@gmail.com>
2021-05-09 20:47:26 +0700

    6s qr reading time

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -22,6 +22,7 @@ import org.opencv.core.CvException;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
@@ -75,7 +76,7 @@ public class YourService extends KiboRpcService {
             QR_snap = api.getMatNavCam();
 
             Bitmap QR_Bitmap = null;
-            Mat QR_mat = imProveImaging(QR_snap, false);
+            Mat QR_mat = imProveImaging(QR_snap, false, true);
             Log.d("IMGPROC", "Bitmap conversion started");
             try {
                 QR_Bitmap = Bitmap.createBitmap(QR_mat.cols(), QR_mat.rows(), Bitmap.Config.ARGB_8888);
@@ -216,11 +217,6 @@ public class YourService extends KiboRpcService {
 
     }
 
-
-
-
-
-
     public AR_result AR_scanAndLocalize(boolean status) {
         int row = 0, col = 0;
         double[][] NavCamInst = api.getNavCamIntrinsics();
@@ -241,7 +237,7 @@ public class YourService extends KiboRpcService {
         Log.d("AR", "Started reading AR");
         while (result == 0) {
             AR_snap = api.getMatNavCam();
-            Mat AR_mat = imProveImaging(AR_snap, false);
+            Mat AR_mat = imProveImaging(AR_snap, false, false);
             try {
                 DetectorParameters parameters = DetectorParameters.create();
 //                parameters.set_errorCorrectionRate(1);
@@ -315,7 +311,7 @@ public class YourService extends KiboRpcService {
     }
 
 
-    public  Mat imProveImaging(Mat source, boolean undistort_state) {
+    public  Mat imProveImaging(Mat source, boolean undistort_state , boolean QR_mode) {
         int row = 0, col = 0;
         double[][] NavCamInst = api.getNavCamIntrinsics();
         Mat cameraMatrix = new Mat(3, 3, CvType.CV_32FC1);
@@ -343,12 +339,25 @@ public class YourService extends KiboRpcService {
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
         List<MatOfPoint> Wanted_contours = new ArrayList<MatOfPoint>();
         Imgproc.findContours(Thres, contours, new Mat(), Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // QR Mode variable //
+        Rect QR_boundingBox = new Rect();
+
         for(int i=0; i< contours.size();i++)
         {
             if ((Imgproc.contourArea(contours.get(i)) >7000))//Imgproc.contourArea(contours.get(i)) > 50
             {
-                Wanted_contours.add(contours.get(i));
-                System.out.println(contours.get(i).dump());
+                if (QR_mode) {
+                    QR_boundingBox = Imgproc.boundingRect(contours.get(i));
+                    if (QR_boundingBox.height > QR_boundingBox.width) {
+                        Wanted_contours.add(contours.get(i));
+                       // System.out.println(contours.get(i).dump());
+                    }
+                } else {
+                    Wanted_contours.add(contours.get(i));
+                    //System.out.println(contours.get(i).dump());
+                }
+
             }
         }
         Log.d("IMGPROC", "Contour location process finished");
@@ -358,8 +367,12 @@ public class YourService extends KiboRpcService {
         Imgproc.fillPoly(mask , Wanted_contours, new Scalar(255, 255, 255));
         Core.bitwise_and(dst,mask,result);
         Log.d("IMGPROC", "Filling process finished");
+        if (QR_mode) {
+            return result.submat(QR_boundingBox.y, QR_boundingBox.y + QR_boundingBox.height, QR_boundingBox.x, QR_boundingBox.x + QR_boundingBox.width);
+        } else {
+            return result;
+        }
 
-        return result;
     }
 
 
